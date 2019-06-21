c8547c6d8c22d0b02a5e7fe8e51d631d86c8cbb5
liuyi <liuxy0551@qq.com>
2019-06-21 17:51:35 +0800

    deploy

--- vue.config.js
+++ vue.config.js
@@ -21,8 +21,9 @@ module.exports = {
    * In most cases please use '/' !!!
    * Detail: https://cli.vuejs.org/config/#publicpath
    */
-  publicPath: '/',
-  outputDir: 'dist',
+  // publicPath: '/',
+  publicPath: process.env.NODE_ENV === 'production'?'/vue-element-admin/': '/',
+  outputDir: 'docs',
   assetsDir: 'static',
   lintOnSave: process.env.NODE_ENV === 'development',
   productionSourceMap: false,
